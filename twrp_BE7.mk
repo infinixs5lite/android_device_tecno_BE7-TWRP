45d98d6b62f0f47979092afce4f92f07f9b8f0c4
infinixs5lite <kelvinchinedu97@gmail.com>
2023-08-05 07:53:56 +0100

    Update twrp_BE7.mk

--- twrp_BE7.mk
+++ twrp_BE7.mk
@@ -5,8 +5,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit from BE7 device
 $(call inherit-product, device/tecno/BE7/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk
 
 PRODUCT_DEVICE := BE7
 PRODUCT_NAME := twrp_BE7
