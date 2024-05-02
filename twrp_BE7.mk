de6d8deb811b485325b1c8ec7e3f16946bc71f90
infinixs5lite <kelvinchinedu97@gmail.com>
2024-05-02 06:17:04 +0100

    Update twrp_BE7.mk

--- twrp_BE7.mk
+++ twrp_BE7.mk
@@ -1,10 +1,8 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
@@ -12,11 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Inherit from BE7 device
 $(call inherit-product, device/tecno/BE7/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := BE7
 PRODUCT_NAME := twrp_BE7
@@ -24,9 +25,3 @@ PRODUCT_BRAND := TECNO pop 6 go
 PRODUCT_MODEL := TECNO BE7
 PRODUCT_MANUFACTURER := tecno
 
-PRODUCT_GMS_CLIENTID_BASE := android-tecno
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_be7_h639-user 11 RP1A.200720.011 276071 release-keys"
-
-BUILD_FINGERPRINT := TECNO/BE7-OP/TECNO-BE7:11/RP1A.200720.011/220413V399:user/release-keys
